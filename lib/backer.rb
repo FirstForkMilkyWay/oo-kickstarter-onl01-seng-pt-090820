65fe5fc2376dc2831e0b28cc281defb37664a362
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-05 23:26:52 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
 
   attr_accessor :name
   
-  
+  @backed_projects = []
   
   def initialize(name)
     @name = name 
@@ -15,6 +15,10 @@ class Backer
   def backed_projects 
     @backed_projects = []
   end 
+  
+  def back_project(Project)
+    @backed_projects << Project 
+  end
 
 
 end 
