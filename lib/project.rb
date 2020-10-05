01c480904906b2035d439bfe303e21546c9ef69c
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-05 23:23:46 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,20 @@
-class Project 
+class Project
 
-
-
+  attr_accessor :title 
+  
+  
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def backed_projects 
+    @backed_projects = []
+  end 
 
 
 end 
